2ff825f36e56fa952fcd6fcc5196eb00a39d4931
Mynsu <43042411+Mynsu@users.noreply.github.com>
2022-03-30 02:38:11 +0900

    init

---
+++ Example_Log4Net/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace Example_Log4Net
+{
+    internal class Program
+    {
+        static ILog log = LogManager.GetLogger("Program");
+        static void Main(string[] args)
+        {
+            log.Debug("디버깅 메시지");
+            log.Info("정보 메시지");
+            log.Warn("경고 메시지");
+            log.Error("에러 메시지");
+            log.Fatal("치명적인 에러 메시지");
+        }
+    }
+}
